96b330296b14ed5bdfdeec1999ccd42521957742
Vlad1235 <fn1235@bk.ru>
2020-03-11 15:16:24 +0900

    Travel_Agency Hibernate

--- src/test/java/task_8/travel_agency2/OrderTest.java
+++ src/test/java/task_8/travel_agency2/OrderTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package task_8.travel_agency2;
 //
 //import org.junit.Assert;
@@ -20,3 +21,31 @@
 //
 //    }
 //}
+=======
+package task_8.travel_agency2;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class OrderTest {
+
+    @Test
+    public void makeOrderSequenceTest(){
+        Client MikeTyson = new Client("Mike","Tyson","8012554433");
+        Hotel InterContinental = new Hotel("InterContinental");
+        Country Mexico = new Country("Mexico");
+        Comments high = new Comments("Level A");
+
+        Tour firstTour = new Tour(Mexico,high,InterContinental);
+        Order madeOrder = new Order(MikeTyson,firstTour);
+        Order result = madeOrder.composedFinalOrder();
+        // проверим что создан экземпляр класса заказ
+        Assert.assertNotNull(result);
+
+    }
+
+
+
+
+}
+>>>>>>> 9f232b9... Travel_Agency Hibernate
